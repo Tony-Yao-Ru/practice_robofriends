d7411f6801d7ffc092ae62b81f30145392c9819c
Tony-Yao-Ru <tonyyu84p0830@gmail.com>
2023-09-11 22:51:57 -0400

    add Hooks and put the onClick() on the card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 const Card = ({ name, email, id }) => {
+  
+  const getUser = () => {
+    console.log(name + email);
+  }
+
   return (
-    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
+    <div onClick={()=>getUser({name}, {email})} className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
       <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,52 +1,61 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import CardList from "../components/CardList";
 import Scroll from '../components/Scroll';
 import SearchBox from "../components/SearchBox";
 import ErrorBoundary from "../components/ErrorBoundary";
 import "../containers/App.css";
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            Robots: [],
-            searchfield: ''
-        };
-        // console.log('constructor');
+function App() {
+// class App extends Component {
+    
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         Robots: [],
+    //         searchfield: ''
+    //     };
+    //     // console.log('constructor');
+    // }
+    const [Robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('')
+
+    const onSearchChange = (event) => {
+        // this.setState({searchfield: event.target.value});
+        setSearchfield(event.target.value);
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value});
-    }
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //         .then(response=> response.json())
+    //         .then(users => {this.setState({Robots: users})});
+    //     // console.log('componentDidMount');
+    // }
+    useEffect(() => {
+            fetch('https://jsonplaceholder.typicode.com/users')
+                .then(response=> response.json())
+                .then(users => {setRobots(users)});
+        }, [] // This is the key for deciding when to use the useEffect()
+    )
 
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response=> response.json())
-            .then(users => {this.setState({Robots: users})});
-        // console.log('componentDidMount');
-    }
-
-    render() {
-        const {Robots, searchfield} = this.state;
-        const filteredRobots = Robots.filter(robot => {
-                return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-            }
+    // const {Robots, searchfield} = this.state;
+    const filteredRobots = Robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+        }
+    );
+    
+    return !Robots.length ? 
+        <h1>Loading</h1>:
+        (
+            <div className='tc'>
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={onSearchChange}/>
+                <Scroll>
+                    <ErrorBoundary> 
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundary>
+                </Scroll>
+             </div>
         );
-        // console.log('render');
-        return !Robots.length ? 
-            <h1>Loading</h1>:
-            (
-                <div className='tc'>
-                    <h1 className="f1">RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange}/>
-                    <Scroll>
-                        <ErrorBoundary>
-                            <CardList robots={filteredRobots} />
-                        </ErrorBoundary>
-                    </Scroll>
-                </div>
-            );
-    }
 }
 
 export default App;
